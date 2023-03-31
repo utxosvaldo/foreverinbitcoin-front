94a2f49a5c3e816198cda2380730435a22ebdc31
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-03-31 02:47:34 -0700

    added all payment status components and logic

--- src/components/InscriptionPreview.svelte
+++ src/components/InscriptionPreview.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte';
+  import { Badge, Button } from 'flowbite-svelte';
   import { Heading } from 'flowbite-svelte';
 
-  import { fileName, priorityFees, feePriority } from '../stores';
+  import {
+    fileName,
+    priorityFees,
+    feePriority,
+    orderStatus,
+    inscriptionId
+  } from '../stores';
 
   export let src: string;
-  export let canCancel: boolean = true;
+  // export let canCancel: boolean = true;
+
+  $: inscriptionLink = `https://ordinals.com/inscription/${$inscriptionId}`;
 
   const cancelPreview = () => {
     $fileName = '';
@@ -13,11 +21,24 @@
 </script>
 
 <!-- <Card img="/test.png" padding="sm" > -->
-<Heading class="text-lg mb-2">Preview</Heading>
+{#if $orderStatus == 8}
+  <Badge border color="purple">Order complete</Badge>
+{:else}
+  <Heading class="text-lg mb-2">Inscription Preview</Heading>
+{/if}
 
-<img {src} class="rounded drop-shadow-lg" alt="Not found" />
+<img {src} class="rounded drop-shadow-lg mb-3" alt="Not found" />
 
-{#if canCancel}
+{#if $orderStatus == 8}
+  <Button
+    gradient
+    color="purpleToPink"
+    class="mt-2 mb-3 w-2/3"
+    href={inscriptionLink}>See your inscription</Button
+  >
+{/if}
+
+{#if $orderStatus < 1}
   <Button outline size="xs" color="purple" class="mb-5" on:click={cancelPreview}
     >Cancel</Button
   >
--- src/components/PaymentStatus.svelte
+++ src/components/PaymentStatus.svelte
@@ -1,65 +1,103 @@
-<script>
+<script lang="ts">
   import { Button, Card } from 'flowbite-svelte';
   import { Spinner } from 'flowbite-svelte';
-  import { CheckCircle } from 'svelte-heros-v2';
-  import { checkoutLink } from '../stores';
+  import { CheckCircle, XCircle, Envelope } from 'svelte-heros-v2';
+  import {
+    checkoutLink,
+    orderStatus,
+    orderId,
+    inscriptionId,
+    inscriptionSentTx
+  } from '../stores';
+
+  // let supportLink = 'mailto:support@foreverinbitcoin.com';
+
+  // $orderStatus = 9;
+  $: receiptLink = $checkoutLink + '/receipt';
+  $: sendTxLink = `https://mempool.space/tx/${$inscriptionSentTx}`;
+  $: supportLink = `mailto:support@foreverinbitcoin.com?subject=ForeverinBitcoin order: ${$orderId}`;
 </script>
 
-<Card class="w-full">
-  <div class=" w-full flex justify-around">
-    <Spinner size="12" color="purple" class="text-center mr-3 " />
-    <div class="w-full grid justify-items-stretch">
-      <strong>Waiting on payment...</strong>
-      <Button gradient color="purpleToPink" class="mt-2" href={$checkoutLink}>
-        Pay in checkout
-      </Button>
-    </div>
-  </div>
-</Card>
+{#if $orderStatus !== 9}
+  {#if $orderStatus == 1}
+    <Card class="w-full">
+      <div class=" w-full flex justify-around">
+        <Spinner size="12" color="purple" class="text-center mr-3 " />
+        <div class="w-full grid justify-items-stretch">
+          <strong>Waiting on payment...</strong>
+          <Button
+            gradient
+            color="purpleToPink"
+            class="mt-2"
+            href={$checkoutLink}
+          >
+            Pay in checkout
+          </Button>
+        </div>
+      </div>
+    </Card>
+  {:else if $orderStatus == 2}
+    <Card class="w-full">
+      <div class=" w-full flex justify-around">
+        <Spinner size="12" color="purple" class="text-center mr-3" />
+        <div class="w-full grid justify-items-stretch">
+          <strong>Payment detected, waiting confirmation...</strong>
+          <Button gradient color="purpleToPink" class="mt-2" href={receiptLink}>
+            See your receipts
+          </Button>
+        </div>
+      </div>
+    </Card>
+  {:else if $orderStatus >= 3}
+    <Card class="w-full">
+      <div class=" w-full flex justify-around">
+        <!-- <Spinner size="12" color="purple" class="text-center mr-3" /> -->
+        <CheckCircle size="50" class="text-purple-700 mr-3" />
+        <div class="w-full grid justify-items-stretch">
+          <strong>Payment confirmed.</strong>
+          <Button outline color="purple" class="mt-2" href={receiptLink}>
+            See your receipt
+          </Button>
+        </div>
+      </div>
+    </Card>
+  {/if}
 
-<Card class="w-full">
-  <div class=" w-full flex justify-around">
-    <Spinner size="12" color="purple" class="text-center mr-3" />
-    <div class="w-full grid justify-items-stretch">
-      <strong>Payment detected, waiting confirmation...</strong>
-      <Button gradient color="purpleToPink" class="mt-2" href={$checkoutLink}>
-        See your receipts
-      </Button>
+  {#if $orderStatus == 4}
+    <Card class="w-full">
+      <div class=" w-full flex justify-around">
+        <Spinner size="12" color="purple" class="text-center mr-3" />
+        <div class="w-full grid justify-items-stretch">
+          <strong>Processing Inscription...</strong>
+        </div>
+      </div>
+    </Card>
+  {:else if $orderStatus == 8}
+    <Card class="w-full">
+      <div class=" w-full flex justify-around">
+        <!-- <Spinner size="12" color="purple" class="text-center mr-3" /> -->
+        <CheckCircle size="50" class="text-purple-700 mr-3" />
+        <div class="w-full grid justify-items-stretch">
+          <strong>Inscription sent to your receive address.</strong>
+          <Button outline color="purple" class="mt-2" href={sendTxLink}>
+            See sent transaction
+          </Button>
+        </div>
+      </div>
+    </Card>
+  {/if}
+{:else}
+  <Card class="w-full">
+    <div class=" w-full flex justify-around">
+      <!-- <Spinner size="12" color="purple" class="text-center mr-3" /> -->
+      <XCircle size="50" class="text-purple-700 mr-3" />
+      <div class="w-full grid justify-items-stretch">
+        <p>Something happened with your order inscription</p>
+        <Button color="purple" class="mt-2" href={supportLink}>
+          <Envelope class="mr-3" />
+          Contact support
+        </Button>
+      </div>
     </div>
-  </div>
-</Card>
-
-<Card class="w-full">
-  <div class=" w-full flex justify-around">
-    <!-- <Spinner size="12" color="purple" class="text-center mr-3" /> -->
-    <CheckCircle size="50" class="text-purple-700 mr-3" />
-    <div class="w-full grid justify-items-stretch">
-      <strong>Payment confirmed.</strong>
-      <Button outline color="purple" class="mt-2" href={$checkoutLink}>
-        See your receipt
-      </Button>
-    </div>
-  </div>
-</Card>
-
-<Card class="w-full">
-  <div class=" w-full flex justify-around">
-    <Spinner size="12" color="purple" class="text-center mr-3" />
-    <div class="w-full grid justify-items-stretch">
-      <strong>Processing Inscription...</strong>
-    </div>
-  </div>
-</Card>
-
-<Card class="w-full">
-  <div class=" w-full flex justify-around">
-    <!-- <Spinner size="12" color="purple" class="text-center mr-3" /> -->
-    <CheckCircle size="50" class="text-purple-700 mr-3" />
-    <div class="w-full grid justify-items-stretch">
-      <strong>Inscription sent to your receive address.</strong>
-      <Button outline color="purple" class="mt-2" href={$checkoutLink}>
-        See sent transaction
-      </Button>
-    </div>
-  </div>
-</Card>
+  </Card>
+{/if}
